a23d0b5f2a414bf40b5c786cb52852cc560a5183
Roman Sinitsyn <roman@WIN-M93GO6H406E.localdomain>
2025-12-16 18:20:37 +0700

    task 10 part 1 is completed

---
+++ task10/task10_1.c
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Parent pid is %d, Parent ppid: %d\n", getpid(), getppid());
+
+    pid_t child_pid = fork();
+
+    if (child_pid == 0) {
+        printf("Child pid: %d, Child ppid: %d\n", getpid(), getppid());
+        exit(EXIT_SUCCESS);
+    } else if (child_pid > 0) {
+        wait(NULL);
+        printf("Child process %d terminated\n", child_pid);
+    } else {
+        printf("fork error");
+    }
+        
+    return 0;
+}
