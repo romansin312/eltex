a3d876b08c38d499c27b4793dee7ddbac7637ca8
Roman Sinitsyn <roman@WIN-M93GO6H406E.localdomain>
2025-12-20 11:54:47 +0700

    task 14, part 1, systemv

---
+++ task14/part1/systemv/server.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+
+#define SHM_KEY 1234
+#define SEM_KEY 5678
+#define BUFFER_SIZE 256
+
+void sem_wait(int semid) {
+    struct sembuf op;
+    op.sem_flg = 0;
+    op.sem_num = 0;
+    op.sem_op = -1;
+    semop(semid, &op, 1);
+}
+
+void sem_signal(int semid) {
+    struct sembuf op;
+    op.sem_flg = 0;
+    op.sem_num = 0;
+    op.sem_op = 1;
+    semop(semid, &op, 1);
+}
+
+int main() {
+    int shmid = shmget(SHM_KEY, BUFFER_SIZE, 0666 | IPC_CREAT);
+    if (shmid == -1) {
+        printf("shmget error\n");
+        return 1;
+    }
+    
+    char *shm_ptr = (char*)shmat(shmid, NULL, 0);
+    if (shm_ptr == (char*)-1) {
+        printf("shmat error\n");
+        return 1;
+    }
+    
+    int semid = semget(SEM_KEY, 1, 0666 | IPC_CREAT);
+    if (semid == -1) {
+        printf("semget\n");
+        return 1;
+    }
+    
+    semctl(semid, 0, SETVAL, 0);
+    
+    strcpy(shm_ptr, "Hi!");
+    printf("Message sent to client\n");
+    sem_signal(semid);
+    
+    printf("Waiting for client response...\n");
+    sem_wait(semid);
+    
+    printf("Received from client: %s\n", shm_ptr);
+    
+    shmdt(shm_ptr);
+    shmctl(shmid, IPC_RMID, NULL);
+    semctl(semid, 0, IPC_RMID);
+    
+    return 0;
+}
