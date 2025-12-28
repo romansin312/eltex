70e087b5ffe09e590e339c9f62f8a7c6aafe974c
Roman <test@test.test>
2025-12-28 14:42:54 +0000

    mutex is used now

--- task20/module2/module2.c
+++ task20/module2/module2.c
@@ -1,44 +1,35 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
-
+#include <linux/mutex.h>
 #define MAX_SIZE 50
+
 static int major = 0;
-static char test_string[MAX_SIZE] = "This is a message from module.";
+static DEFINE_MUTEX(lock);
+static char test_string[MAX_SIZE] = "This is a message from module.\0";
 
 ssize_t test_read(struct file *fd, char __user *buff, size_t size, loff_t *off)
 {
-    size_t len = strlen(test_string);
-    
-    pr_info("module2: test_read: size=%zu, off=%lld, len=%zu\n", size, *off, len);
-    
-    if (*off >= len)
-        return 0;
-    
-    if (size > len - *off)
-        size = len - *off;
-    
-    if (copy_to_user(buff, test_string + *off, size))
-        return -EFAULT;
-    
-    *off += size;
-    return size;
+    size_t rc;
+
+    mutex_lock(&lock);
+    rc = simple_read_from_buffer(buff, size, off, test_string, MAX_SIZE);
+    mutex_unlock(&lock);
+
+    return rc;
 }
 
 ssize_t test_write(struct file *fd, const char __user *buff, size_t size, loff_t *off)
 {
+    size_t rc = 0;
     if (size > MAX_SIZE)
         return -EINVAL;
-    
-    if (copy_from_user(test_string, buff, size))
-        return -EFAULT;
-    
-    if (size < MAX_SIZE)
-        test_string[size] = '\0';
-    
-    pr_info("module2: test_write: wrote %zu bytes\n", size);
-    
-    return size;
+
+    mutex_lock(&lock);
+    rc = simple_write_to_buffer(test_string, MAX_SIZE, off, buff, size);
+    mutex_unlock(&lock);
+
+    return rc;
 }
 
 static struct file_operations fops = {
@@ -49,20 +40,19 @@ static struct file_operations fops = {
 
 int init_module(void)
 {
-    pr_info("module2: Test module is loaded.\n");
+    pr_info("Test module is loaded.\n");
     major = register_chrdev(major, "module2", &fops);
-    
+
     if (major < 0)
         return major;
-    
-    pr_info("module2: Major number is %d.\n", major);
+    pr_info("Major number is %d.\n", major);
+
     return 0;
 }
 
 void cleanup_module(void)
 {
     unregister_chrdev(major, "module2");
-    pr_info("module2: Module unloaded.\n");
 }
 
 MODULE_LICENSE("GPL");
